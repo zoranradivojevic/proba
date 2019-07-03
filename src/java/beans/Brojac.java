5f5bc3a5c361660f80e606d9bf0ee1417d57f230
Obuka <Obuka@DESKTOP-F6T8J5C>
2019-07-03 18:51:58 +0200

    Revert "verzija 1"
    This reverts commit 94402b806a3babc8bfd02619d07f813b49b3d8a3.

---
+++ src/java/beans/Brojac.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Obuka
+ */
+@Named(value="brojac")
+@Dependent
+public class Brojac {
+
+    public Brojac() {
+    }
+    
+    public String poruka () {
+        return "Poruka iz brojaca 1";
+    }
+    
+}
